50c2c6de8b7d585615239b64a58b1fd7755c510f
Barox <75582570+baroxFPTU@users.noreply.github.com>
2021-04-28 00:05:45 +0700

    complet the first algorithm break line.

--- script.js
+++ script.js
@@ -3,25 +3,43 @@ const $ = document.querySelector.bind(document);
 const submitBtn = $('.btn-submit');
 const textarea = $('.submit-input');
 const app = {
+    limitLetter: [8,10],// the first value is the value of first line, and the other is the value of second line.
+    sumLetter: function() {
+        return this.limitLetter.reduce((acc,cur) => acc + cur,0);
+    },
     handleEvents: function() {
-        _this = this;
+        const _this = this;
+
         submitBtn.addEventListener('click',(event) => {
             event.preventDefault();
-            const subtitles = textarea.value.replace(/\r?\n/g, ' ');
+            const subtitles = textarea.value.replace(/\r?\n/g, ' '); //flat the string, delete the '/n' letter
+            const arr = [];
             if (subtitles) {
-                _this.breakLine(subtitles);
+                const arrWords = subtitles.split(' '); //split every letter by the spaces
+
+                while (arrWords.length > 0) {
+                    arr.push(_this.breakLine(arrWords));
+                    arrWords.splice(0, _this.sumLetter());
+                }
+
+                arr.forEach(value => console.log(value));
             } else {
                 alert('Please enter your subtitles.');
             }
 
         })
     },
-    breakLine: function(value) {
-        const limitLetter = [8,10];
-        if (value) {
-            const arrWords = value.split(' ');
-            console.log(arrWords.slice(0,limitLetter[0]).join(' ')+ '\n' + arrWords.slice(limitLetter[0], limitLetter[0] + limitLetter[1]).join(' '));
-        }
+    breakLine: function(arr) {
+        if (arr) {
+            
+            const firstLine = arr.splice(0,this.limitLetter[0]).join(' ').trim();
+            const secondLine = arr.slice(this.limitLetter[0], this.limitLetter[0] + this.limitLetter[1]).join(' ').trim();
+            const breakLine = `${firstLine}\n${secondLine}` ;
+
+            return breakLine;
+        } else {
+            console.error('Something was wrong.')
+        };
     }, 
     start: function() {
         this.handleEvents(); //Handle all DOM events on the page
